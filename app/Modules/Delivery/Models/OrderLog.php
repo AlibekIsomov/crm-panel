6f063912249f6b922e534c448c30c91665dd721a
Ali Isomov <aisomov.dev@gmail.com>
2026-02-12 16:05:57 +0500

    feat: Add delivery module with migration/model

---
+++ app/Modules/Delivery/Models/OrderLog.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\Delivery\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderLog extends Model
+{
+    //
+}
---
+++ database/migrations/2026_02_12_104812_create_order_logs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('service'); // geocoder, routing, payment, sms
+            $table->string('method');
+            $table->text('url');
+            $table->json('request_body')->nullable();
+            $table->json('response_body')->nullable();
+            $table->integer('status_code');
+            $table->integer('duration_ms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_logs');
+    }
+};
